0513a63e3ae49d9b33a427006f5c0a14db59bf0d
Petr Šulc <95595719+Petross808@users.noreply.github.com>
2023-11-08 16:30:41 +0000

    another example of different lambda

--- src/main/java/ActivityOrder.java
+++ src/main/java/ActivityOrder.java
@@ -5,7 +5,17 @@ public enum ActivityOrder {
     CALORIES_DESC(CALORIES_ASC.comparator.reversed()),
     DATE_ASC(Comparator.naturalOrder()),
     DATE_DESC(DATE_ASC.comparator.reversed()),
-    DURATION_ASC (Comparator.comparing(Activity::getDuration)),
+    //Example of lambda sort
+    DURATION_ASC ((a, b) -> {
+                        {
+                            if(a.getDuration() < b.getDuration())
+                                return -1;
+                            else if(a.getDuration() > b.getDuration())
+                                return 1;
+                            else
+                                return 0;
+                        }
+                    }),
     DURATION_DESC (DURATION_ASC.comparator.reversed()),
     TYPE_ASC(new TypeComparator()), // Example use of Comparator class
     TYPE_DESC(TYPE_ASC.comparator.reversed()),
